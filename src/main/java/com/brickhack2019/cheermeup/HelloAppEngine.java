8d13ba8ff6b00018810c895d021e47e9360100e2
David Dunlap <ductdunlap@gmail.com>
2019-02-16 22:58:23 -0500

    small webcam changes

--- src/main/java/com/brickhack2019/cheermeup/HelloAppEngine.java
+++ src/main/java/com/brickhack2019/cheermeup/HelloAppEngine.java
@@ -2,13 +2,18 @@ package com.brickhack2019.cheermeup;
 
 import com.google.appengine.api.utils.SystemProperty;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.util.Properties;
 
+import javax.imageio.ImageIO;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.github.sarxos.webcam.Webcam;
 
 @WebServlet(name = "HelloAppEngine", value = "/hello")
 public class HelloAppEngine extends HttpServlet {
@@ -28,6 +33,15 @@ public class HelloAppEngine extends HttpServlet {
     return "Version: " + System.getProperty("java.version")
           + " OS: " + System.getProperty("os.name")
           + " User: " + System.getProperty("user.name");
+  }
+
+  public static String handleWebcam() throws IOException {
+    Webcam webcam = Webcam.getDefault();
+    webcam.open();
+    BufferedImage image = webcam.getImage();
+    ImageIO.write(webcam.getImage(), "PNG", new File("hello-world.png"));
+    System.out.println("Success");
+    return "";
   }
 
 }
